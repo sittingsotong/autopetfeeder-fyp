991e4ad27226c1ded320accd1cf34057f4f446e3
siting <sz2619@ic.ac.uk>
2022-05-12 17:21:16 +0100

    Working motor with firebase

--- pi/motor.py
+++ pi/motor.py
@@ -1,74 +1,79 @@
 import RPi.GPIO as GPIO
 import time
 
-in1 = 17
-in2 = 18
-in3 = 27
-in4 = 22
+class Motor:
+    def __init__(self):
+        self.in1 = 17
+        self.in2 = 18
+        self.in3 = 27
+        self.in4 = 22
 
-# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-step_sleep = 0.002
+        # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+        self.step_sleep = 0.002
 
-step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
+        self.step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
 
-direction = False # True for clockwise, False for counter-clockwise
+        self.direction = False # True for clockwise, False for counter-clockwise
 
-# defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
-step_sequence = [[1,0,0,1],
-                 [1,0,0,0],
-                 [1,1,0,0],
-                 [0,1,0,0],
-                 [0,1,1,0],
-                 [0,0,1,0],
-                 [0,0,1,1],
-                 [0,0,0,1]]
- 
-# setting up
-GPIO.setmode( GPIO.BCM )
-GPIO.setup( in1, GPIO.OUT )
-GPIO.setup( in2, GPIO.OUT )
-GPIO.setup( in3, GPIO.OUT )
-GPIO.setup( in4, GPIO.OUT )
+        # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+        self.step_sequence = [[1,0,0,1],
+                        [1,0,0,0],
+                        [1,1,0,0],
+                        [0,1,0,0],
+                        [0,1,1,0],
+                        [0,0,1,0],
+                        [0,0,1,1],
+                        [0,0,0,1]]
+        
+        # setting up
+        GPIO.setmode( GPIO.BCM )
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.in3, GPIO.OUT)
+        GPIO.setup(self.in4, GPIO.OUT)
 
-# initializing
-GPIO.output( in1, GPIO.LOW )
-GPIO.output( in2, GPIO.LOW )
-GPIO.output( in3, GPIO.LOW )
-GPIO.output( in4, GPIO.LOW )
+        # initializing
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
 
-
-motor_pins = [in1,in2,in3,in4]
-motor_step_counter = 0 ;
-
-
-def cleanup():
-    GPIO.output( in1, GPIO.LOW )
-    GPIO.output( in2, GPIO.LOW )
-    GPIO.output( in3, GPIO.LOW )
-    GPIO.output( in4, GPIO.LOW )
-    GPIO.cleanup()
+        self.motor_pins = [self.in1,self.in2,self.in3,self.in4]
+        self.motor_step_counter = 0
 
 
-# the meat
-try:
-    i = 0
-    for i in range(step_count):
-        for pin in range(0, len(motor_pins)):
-            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
-        if direction==True:
-            motor_step_counter = (motor_step_counter - 1) % 8
-        elif direction==False:
-            motor_step_counter = (motor_step_counter + 1) % 8
-        else: # defensive programming
-            print( "uh oh... direction should *always* be either True or False" )
-            cleanup()
-            exit( 1 )
-        time.sleep( step_sleep )
+    def cleanup(self):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
+        GPIO.cleanup()
 
-except KeyboardInterrupt:
-    cleanup()
-    exit( 1 )
 
-cleanup()
-exit( 0 )
+    def rotate(self, amt):
+        rotate_amt = int(amt/100 * self.step_count)
 
+        try:
+            i = 0
+            for i in range(rotate_amt):
+                for pin in range(0, len(self.motor_pins)):
+                    GPIO.output(self.motor_pins[pin], self.step_sequence[self.motor_step_counter][pin])
+                if self.direction==True:
+                    self.motor_step_counter = (self.motor_step_counter - 1) % 8
+                elif self.direction==False:
+                    self.motor_step_counter = (self.motor_step_counter + 1) % 8
+                else: # defensive programming
+                    print("uh oh... direction should *always* be either True or False")
+                    self.cleanup()
+                    exit(1)
+                
+                time.sleep(self.step_sleep)
+
+        except KeyboardInterrupt:
+            self.cleanup()
+            exit(1)
+
+if __name__ == '__main__':
+    motor = Motor()
+    motor.rotate(50)
+    motor.cleanup()
